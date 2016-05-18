3570d3aae13dd5ba200cc2ad5f9a20173446e6c0
cullorblind <ke5kul@amarillowireless.net>
2016-05-18 17:52:55 -0500

    Fix hostnames
    Moved hosts

--- mcManage-rsync.sh
+++ mcManage-rsync.sh
@@ -14,13 +14,13 @@ touch ${HOME}
 SCREENSESSION='mcmyadmin'
 
 #check server, pause saving, save, backup, and resume saving.
-nc mc1.ke5kul.prwn.net 25565 < /dev/null
+nc mc1.prwn.net 25565 < /dev/null
 if [ $? = 0 ] #check if server is running
 then #Inform players that backup started
 #   /usr/bin/screen -S ${SCREENSESSION} -X stuff "say ###Cullor Rsync Backup - Started###"`echo -ne '\015'`
    /usr/bin/screen -S ${SCREENSESSION} -X stuff "save-off"`echo -ne '\015'`
    /usr/bin/screen -S ${SCREENSESSION} -X stuff "save-all"`echo -ne '\015'`
-   /usr/bin/rsync -avr --delete --progress --stats ${HOME} backups.ke5kul.prwn.net::mc1/0-latest/
+   /usr/bin/rsync -avr --delete --progress --stats ${HOME} backups.prwn.net::mc1/0-latest/
    /usr/bin/screen -S ${SCREENSESSION} -X stuff "save-on"`echo -ne '\015'`
 #   /usr/bin/screen -S ${SCREENSESSION} -X stuff "say ###Cullor Rsync Backup - Complete###"`echo -ne '\015'`
    exit 0
